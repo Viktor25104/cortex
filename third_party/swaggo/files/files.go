96efa907a85cc48fed264902f1e33f485dbe465e
Viktor25104 <101422976+Viktor25104@users.noreply.github.com>
2025-11-03 21:03:54 +0200

    chore: remove stub dependencies and refresh build setup

--- third_party/swaggo/files/files.go
+++
@@ -1,7 +0,0 @@
-package files
-
-import "net/http"
-
-var Handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	http.NotFound(w, r)
-})
--- third_party/swaggo/swag/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/swaggo/swag
-
-go 1.25
